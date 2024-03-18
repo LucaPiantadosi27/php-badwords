bb464d354eb41ba51da939b846e61a84260688b3
Luca Piantadosi <lu.piantadosi125@gmail.com>
2024-03-18 13:16:17 +0100

    esercizio finito

--- index.php
+++ index.php
@@ -21,20 +21,28 @@ Gestire il tutto con due file diversi.
 P. S.
 Per debuggare la vostra pagina utilizzate la funzione var_dump(), ma una volta completato l'esercizio fate sparire le stampe di debug, scrivete tutto con echo e in maniera ordinata e piacevole per l'utente -->
 
-<body>
-    <div class="container">
-        <h1>BAD WORDS CENSOR</h1>
-        <form action="" method="get">
-        <label for="Frase"></label>
-        <?php
-        echo "TEST"
-        ?>
-        <input type="text" name="Frase" id="Frase">
-        <label for="Frase"></label>
-        <input type="text" name="Frase" id="Frase">
-        </form>
 
+<body>
+    <div class="container py-4 ">
+        <h1>PHP Bad-words Censor</h1>
+
+        <form action="censor.php">
+            <div class="mb-3">
+                <label for="paragraph" class="form-label">Paragrafo</label>
+                <textarea class="form-control" name="paragraph" id="paragraph" rows="3"></textarea>
+            </div>
+            <div class="mb-3">
+                <label for="censor-word" class="form-label">Parola da censurare</label>
+                <input type="text" name="censor-word" class="form-control" id="censor-word" placeholder="***">
+            </div>
+
+
+            <button type="submit" class="btn btn-primary">Invia</button>
+        </form>
     </div>
+
+    <!-- bootstrap -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 
 </body>
 </html>
